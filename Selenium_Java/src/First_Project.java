6d00179ea818fae30bbaa9492746c30b11189043
keerthiv <keerthivsanjay6@gmail.com>
2022-08-12 17:42:13 +0530

    seperate folder check commit

---
+++ Selenium_Java/src/First_Project.java
@@ -0,0 +1,105 @@
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First_Project implements WebDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Admin\\eclipse-workspace\\Selenium_Java\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://mail.google.com/");
+		System.out.println("the title is " + driver.getTitle());
+		driver.findElement(By.id("identifierId")).sendKeys("keerthivsanjay@gmail.com");
+		driver.findElement(By.xpath(
+				"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span"))
+				.click();
+
+	}
+
+	@Override
+	public void get(String url) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getCurrentUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<WebElement> findElements(By by) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WebElement findElement(By by) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPageSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void quit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<String> getWindowHandles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWindowHandle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TargetLocator switchTo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Navigation navigate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Options manage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
